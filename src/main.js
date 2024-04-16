57348223d6486a477ac08d9fef7ced0f4e2bd66d
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-04-16 12:33:30 +0300

    індикатор завантаження з css-loader

--- src/main.js
+++ src/main.js
@@ -26,3 +26,35 @@ searchForm.addEventListener('submit', async (event) => {
 });
 
 
+
+
+
+
+searchForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const searchInput = document.getElementById('search-input');
+  const searchQuery = searchInput.value.trim();
+  const loader = document.getElementById('loader'); // Отримання елемента спінера
+
+  if (!searchQuery) {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search query!',
+    });
+    return;
+  }
+
+  // Показати спінер перед відправкою запиту
+  loader.classList.remove('hidden');
+
+  try {
+    const images = await fetchImages(searchQuery);
+    renderImages(images);
+  } catch (error) {
+    console.error('Error searching for images:', error);
+  } finally {
+    // Сховати спінер після завершення запиту
+    loader.classList.add('hidden');
+  }
+});
