30cd61fa2ed0c26c70178d73ecd9503b1ab466fd
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-04-18 13:08:24 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,40 +2,13 @@ import { fetchImages } from './js/pixabay-api.js';
 import { renderImages } from './js/render-functions.js';
 
 const searchForm = document.getElementById('search-form');
+const loader = document.getElementById('loader'); // Отримання елемента спінера
 
 searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const searchInput = document.getElementById('search-input');
   const searchQuery = searchInput.value.trim();
-
-  if (!searchQuery) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Please enter a search query!',
-    });
-    return;
-  }
-
-  try {
-    const images = await fetchImages(searchQuery);
-    renderImages(images);
-  } catch (error) {
-    console.error('Error searching for images:', error);
-  }
-});
-
-
-
-
-
-
-searchForm.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  const searchInput = document.getElementById('search-input');
-  const searchQuery = searchInput.value.trim();
-  const loader = document.getElementById('loader'); // Отримання елемента спінера
 
   if (!searchQuery) {
     iziToast.error({
